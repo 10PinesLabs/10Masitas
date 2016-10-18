741fcf7413a43e8e561cb533c8e5d972e71ea57e
GianFF <gf.fioriello@gmail.com>
2016-10-18 16:49:38 -0200

    Add MVC project structure

---
+++ 10Masitas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_10Masitas.Startup))]
+namespace _10Masitas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
