fdc50afcb32b6c595c600de821bf5735b556afe0
GianFF <gf.fioriello@gmail.com>
2016-10-18 17:23:42 -0200

    Create basic order

---
+++ 10Masitas.Tests/Models/OrderTest.cs
@@ -0,0 +1,16 @@
+﻿using _10Masitas.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _10Masitas.Tests.Models
+{
+    [TestClass]
+    public class OrderTest
+    {
+        [TestMethod]
+        public void WhenCreateAnOrderThenIsActive()
+        {
+            var order = new Order();
+            Assert.IsTrue(order.IsActive());
+        }
+    }
+}
---
+++ 10Masitas/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace _10Masitas.Models
+{
+    public class Order
+    {
+        public Order()
+        {
+        }
+
+        public bool IsActive()
+        {
+            return true;
+        }
+    }
+}
